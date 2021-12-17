42aa7138bd674da11cc1e87342c722faa7aa2142
Lothering0 <danil2005232@gmail.com>
2021-12-17 19:30:51 +0300

    Changed origin in cors

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express()
 app.use(cookieParser())
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:3000']
+  origin: [process.env.FRONTEND_ADDRESS]
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
